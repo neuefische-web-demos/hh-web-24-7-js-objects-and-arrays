fa36d34aaf1a694d6c7ab5824327d4b32557b7a5
Andrea Hohnsen <a.hohnsen@mail.de>
2024-07-09 11:09:17 +0200

    Add demo for objects

--- script.js
+++ script.js
@@ -37,20 +37,72 @@ console.log(shoppingList);
 // Declaring an object
 // const objectName = { key1: value1, key2: value2, ...}
 
+// const person = {
+//   name: "Max Blub",
+//   age: 21,
+//   isStudent: false,
+// };
+
 // Accessing property via dot notation
+// console.log(person.name);
+// console.log(person.age);
 
 // Accessing property via bracket notation
+// console.log(person["name"]);
+// console.log(person["age"]);
 
 // Nesting object in another object
+const person = {
+  name: "Max Blub",
+  age: 21,
+  isStudent: false,
+  address: {
+    street: "Berliner Str.",
+    houseNumber: 42,
+    city: "Leipzig",
+    zipCode: "12345",
+  },
+};
 
 // Accessing property in nested object
+console.log(person.address.street);
 
 // Manipulating objects
 
 // Overwrite existing property
+person.name = "Max Bla";
+console.log(person);
+person["name"] = "Max Blub";
+console.log(person.name);
 
 // Add new property
+person.score = 15;
+console.log(person);
 
 // Delete property
+delete person.score;
+console.log(person);
 
 // Nested objects and arrays
+const peopleArray = [
+  {
+    name: "John",
+    age: 22,
+  },
+  {
+    name: "Alex",
+    age: 33,
+  },
+];
+
+console.log(peopleArray[1].name);
+console.log(peopleArray[1]["name"]);
+
+const user = {
+  userId: "1234",
+  mail: "test@mail.com",
+  shoppingCart: ["tomato", "banana", "chocolate"],
+};
+
+console.log(user.shoppingCart[0]);
+console.log(user["shoppingCart"][0]);
