8538be5534d3f50be27b055effa3fe7f5dae2957
Andrea Hohnsen <a.hohnsen@mail.de>
2024-07-09 10:07:50 +0200

    Add demo for arrays

--- script.js
+++ script.js
@@ -2,22 +2,35 @@
 
 // Declaring an array
 // const arrayName = [item1, item2, item3, ...]
+//                      0         1
+const shoppingList = ["apple", "tomato"];
 
 // Accessing array element via bracket notation and the item's index
+console.log(shoppingList[0]);
+console.log(shoppingList[1]);
 
 // Getting the length of an array
+console.log(shoppingList.length);
 
 // Nesting array in another array
+const complexArray = [1, 2, ["hello", false], true];
 
 // Accessing element in nested array
+console.log(complexArray[2][1]);
 
 // Manipulating arrays
 
 // Overwrite value in array
+shoppingList[0] = "banana";
+console.log(shoppingList);
 
 // Append data to end of array
+shoppingList.push("orange");
+console.log(shoppingList);
 
 // Remove data from the end of array
+shoppingList.pop();
+console.log(shoppingList);
 
 // OBJECTS
 
